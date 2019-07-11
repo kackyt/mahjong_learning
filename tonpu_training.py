834a8e27cf685d0f543075c448934579eea58d5e
kacky <t_kaki@nextappli.com>
2019-07-11 20:38:30 +0900

    new: 役を正しくDBに入力する

--- tonpu_training.py
+++ tonpu_training.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 import numpy as np
 from pymongo import MongoClient
 from mahjong_lib import search_agari
@@ -28,10 +29,10 @@ pipeline = [
       "_id" :0,
       "result": {
         "$reduce": {
-          "input": "$kyokus.states",
+          "input": "$kyokus",
           "initialValue": [],
           "in": {
-            "$concatArrays": ["$$value", "$$this"]
+            "$concatArrays": ["$$value", ["$$this.states"]]
           }
         }
       }
@@ -42,11 +43,9 @@ pipeline = [
 collection = db.haifu
 
 results = collection.aggregate(pipeline = pipeline)
-kyoku_num = collection.estimated_document_count()
-# ランダムな局をテスト用として使う
-test_idx = np.random.randint(0, kyoku_num)
+doc_num = collection.estimated_document_count()
+didx = 0
 kidx = 0
-print(test_idx)
 
 for result in results:
   if np.random.random() <= 0.01:
@@ -57,59 +56,71 @@ for result in results:
     output_yomi = train_yomi_writer
     output_yomi_rnn = train_yomi_rnn_writer
     output_tehai = train_tehai_writer
-  states = result['result']
-  states_num = len(states)
-  kidx += 1
-  print("process (", kidx, "/", kyoku_num, ")")
+  kyokus = result['result']
+  kyoku_num = len(kyokus)
+  didx += 1
+  print("process (", didx, "/", doc_num, ")")
 
-  for state in states:
-    players = state['players']
-    action = state['action']
-    player_num = len(players)
-    if action['type'] == 'dahai':
-      yomi_idx = action['self'] -1
-    else:
-      yomi_idx = -1
+  for kyoku in kyokus:
+    state_num = [0, 0, 0, 0]
+    state_pos = [0, 0, 0, 0]
+    for state in kyoku:
+      action = state['action']
+      if action['type'] == 'dahai':
+        state_num[action['self']-1] += 1
+    for state in kyoku:
+      players = state['players']
+      action = state['action']
+      player_num = len(players)
+      if action['type'] == 'dahai':
+        yomi_idx = action['self'] -1
+      else:
+        yomi_idx = -1
 
-    for pidx in range(player_num):
-      player = players[pidx]
-      hais = np.zeros(34, dtype=np.int32)
-      kawa = np.zeros(35, dtype=np.int32)
-      if yomi_idx != pidx:
+      for pidx in range(player_num):
+        player = players[pidx]
+        hais = np.zeros(34, dtype=np.int32)
+        kawa = np.zeros(35, dtype=np.int32)
+        if yomi_idx != pidx:
           continue
-      try:
-        for hai in player['hais']:
-          hais[hai['no']] += 1
-
-        for naki in player['naki']:
-          for hai in naki['hais']:
+        try:
+          for hai in player['hais']:
             hais[hai['no']] += 1
 
-        for kw in player['kawa']:
-          kawa[kw['no']] += 1
-          if kw['tsumogiri']:
-            kawa[34] += 1
+          for naki in player['naki']:
+            for hai in naki['hais']:
+              hais[hai['no']] += 1
 
-        agari = search_agari(hais)
-        l = [ agari[3] ]
-        l.extend(kawa.tolist())
-        output_yomi.writerow(l)
-        l = [ agari[3] ]
-        l.extend(hais.tolist())
-        output_tehai.writerow(l)
-        haivec = np.zeros(35, dtype=np.int32)
-        haivec[action['hais'][0]['no']] += 1
-        if action['tsumogiri']:
-            haivec[34] = 1
-        l = [kidx, pidx, agari[3]]
-        l.extend(haivec.tolist())
-        output_yomi_rnn.writerow(l)
-      except TypeError:
-        pass
-      else:
-        pass
+          for kw in player['kawa']:
+            kawa[kw['no']] += 1
+            if kw['tsumogiri']:
+              kawa[34] += 1
+
+          agari = search_agari(hais)
+          l = [ agari[3] ]
+          l.extend(kawa.tolist())
+          output_yomi.writerow(l)
+          l = [ agari[3] ]
+          l.extend(hais.tolist())
+          output_tehai.writerow(l)
+          if state_num[yomi_idx] >= 10 and state_pos[yomi_idx] <= 10:
+            haivec = np.zeros(35, dtype=np.int32)
+            haivec[action['hais'][0]['no']] += 1
+            if action['tsumogiri']:
+              haivec[34] = 1
+            l = [kidx*4+pidx, agari[3]]
+            l.extend(haivec.tolist())
+            output_yomi_rnn.writerow(l)
+            state_pos[yomi_idx] += 1
+        except TypeError:
+          pass
+        else:
+          pass
+    kidx += 1
 
 tehai_training.close()
 yomi_training.close()
+yomi_rnn_training.close()
 tehai_test.close()
 yomi_test.close()
+yomi_rnn_test.close()
